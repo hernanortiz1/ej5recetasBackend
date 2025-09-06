3af427d30ec3e6f187edefded6c2e076e78e7237
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-09-05 23:15:02 -0300

    agregue logica a creacReceta

--- src/controllers/receta.controllers.js
+++ src/controllers/receta.controllers.js
@@ -4,3 +4,25 @@ export const test = (req, res) => {
   res.status(200);
   res.send("Primera prueba desde el backend");
 };
+
+export const leerReceta = async (req, res) => {
+  try {
+    const listaRecetas = await Receta.find({});
+    res.status(200).json(listaRecetas);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensaje: "Error  al leer receta" });
+  }
+};
+
+export const crearReceta = async (req, res) => {
+  try {
+    const nuevaReceta = new Receta(req.body);
+    await nuevaReceta.save();
+
+    res.status(201).json({ mensaje: "La Receta fue creada exitosamente" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensaje: "Error al crear receta" });
+  }
+};
