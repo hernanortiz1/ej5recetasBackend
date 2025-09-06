5bdf299f061e5353c6b3bc5a1012e0b3646af99a
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-09-05 23:23:31 -0300

    agregue ruta

--- src/routes/receta.routes.js
+++ src/routes/receta.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { crearReceta, leerReceta, test } from "../controllers/receta.controllers.js";
+import { crearReceta, leerReceta, test, leerRecetaPorId } from "../controllers/receta.controllers.js";
 
 const router = Router();
 
 router.route("/test").get(test);
 router.route("/").get(leerReceta).post(crearReceta)
+router.route("/:id").get(leerRecetaPorId)
 export default router;
