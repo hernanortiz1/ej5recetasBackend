d9d159c2db07616f30f26cfeefd4f6358912904d
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-09-08 11:34:48 -0300

    agregue funcion editar receta

--- src/routes/receta.routes.js
+++ src/routes/receta.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { crearReceta, leerReceta, test, leerRecetaPorId, borrarRecetaPorId } from "../controllers/receta.controllers.js";
+import { crearReceta, leerReceta, test, leerRecetaPorId, borrarRecetaPorId, editarRecetaPorId } from "../controllers/receta.controllers.js";
 
 const router = Router();
 
 router.route("/test").get(test);
 router.route("/").get(leerReceta).post(crearReceta)
-router.route("/:id").get(leerRecetaPorId).delete(borrarRecetaPorId)
+router.route("/:id").get(leerRecetaPorId).delete(borrarRecetaPorId).put(editarRecetaPorId)
 export default router;
