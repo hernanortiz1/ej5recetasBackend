c720becad83d762e79762e0c0548fc7cc5780e2e
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-09-14 00:12:16 -0300

    agregue validacion de token en crear y editar

--- src/routes/receta.routes.js
+++ src/routes/receta.routes.js
@@ -1,10 +1,26 @@
 import { Router } from "express";
-import { crearReceta, leerReceta, test, leerRecetaPorId, borrarRecetaPorId, editarRecetaPorId } from "../controllers/receta.controllers.js";
+import {
+  crearReceta,
+  leerReceta,
+  test,
+  leerRecetaPorId,
+  borrarRecetaPorId,
+  editarRecetaPorId,
+} from "../controllers/receta.controllers.js";
 import validacionReceta from "../middleware/validarReceta.js";
+import verificarJWT from "../middleware/verificarJWT.js";
 
 const router = Router();
 
 router.route("/test").get(test);
-router.route("/").get(leerReceta).post(validacionReceta ,crearReceta)
-router.route("/:id").get(leerRecetaPorId).delete(borrarRecetaPorId).put(validacionReceta ,editarRecetaPorId)
+router
+  .route("/")
+  .get(leerReceta)
+  .post([verificarJWT, validacionReceta], crearReceta);
+router
+  .route("/:id")
+  .get(leerRecetaPorId)
+  .delete(verificarJWT,borrarRecetaPorId)
+  .put([verificarJWT,validacionReceta], editarRecetaPorId);
+
 export default router;
