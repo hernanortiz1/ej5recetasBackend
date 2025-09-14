784b89ca931ebc8eb6b78f0697c6992c17acee79
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-09-14 00:44:47 -0300

    corregi ruta de usuario

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,8 +1,10 @@
 import recetaRoutes from "./receta.routes.js";
 import { Router } from "express";
+import usuariosRoutes from "./usuario.routes.js"
 
 const router = Router();
 
 router.use("/recetas", recetaRoutes);
+router.use("/usuarios", usuariosRoutes)
 
 export default router;
